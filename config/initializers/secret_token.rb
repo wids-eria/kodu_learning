d7016659c335c54e288ff06b0314b4fe6a03ff72
Stephen Anderson and Shree Durga <dev+bendycode+sdurga@bendyworks.com>
2012-01-17 10:31:06 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoduLearning::Application.config.secret_token = '7d964038cb758da3c30005f6cd869044a86f41b9a8a47e00a19913bb38b989a3b7a5737b909d171b7f9824847cb152437a7083524865498770dce0f3f75528e0'
