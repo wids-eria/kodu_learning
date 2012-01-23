bd3bf5b78cde2382807676c07858d7a8fc805a9a
Kevin Alford <zeroeth@pixelflow.org>
2012-01-23 13:59:31 -0600

    devise for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 KoduLearning::Application.routes.draw do
+  get "user/sign_in"
+
+  get "user/sign_out"
+
+  devise_for :users
+
   resources :levels
 
   # The priority is based upon order of creation:
@@ -50,7 +56,7 @@ KoduLearning::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'levels#index'
+  root :to => '#index'
 
   # See how all your routes lay out with "rake routes"
 
