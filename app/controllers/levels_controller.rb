0876f30f33d9c5fcf8784d905ab4a349d5cdc92e
Shree Durga <shree.durga@gmail.com>
2012-01-24 11:28:47 -0600

    user authentication for levels

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,6 +1,7 @@
 class LevelsController < ApplicationController
   # GET /levels
   # GET /levels.json
+  before_filter :authenticate_user!
   def index
     @levels = Level.all
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 KoduLearning::Application.routes.draw do
-  get "user/sign_in"
-
-  get "user/sign_out"
-
+  
   devise_for :users
 
   resources :levels
@@ -56,7 +53,7 @@ KoduLearning::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => '#index'
+  root :to => 'levels#index'
 
   # See how all your routes lay out with "rake routes"
 
