4aee228ebbede1be0e58618f4ad698cabf872ca0
Kevin Alford <zeroeth@pixelflow.org>
2012-01-30 09:07:45 -0600

    Remove rspec valid_session which was breaking devise

--- spec/controllers/levels_controller_spec.rb
+++ spec/controllers/levels_controller_spec.rb
@@ -5,21 +5,17 @@ describe LevelsController do
     user = User.new :email => 'test@somewhere.com', :password => 'password'
     user.save!
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    sign_in user
+    sign_in :user, user
   end
   
   def valid_attributes
     { name: 'My Awesome Level', level_file_file_name: 'my_awesome_file_name' }
   end
 
-  def valid_session
-    {}
-  end
-
   describe "GET index" do
     it "assigns all levels as @levels" do
       level = Level.create! valid_attributes
-      get :index, {}, valid_session
+      get :index
       assigns(:levels).should eq([level])
     end
   end
@@ -27,14 +23,14 @@ describe LevelsController do
   describe "GET show" do
     it "assigns the requested level as @level" do
       level = Level.create! valid_attributes
-      get :show, {:id => level.to_param}, valid_session
+      get :show, {:id => level.to_param}
       assigns(:level).should eq(level)
     end
   end
 
   describe "GET new" do
     it "assigns a new level as @level" do
-      get :new, {}, valid_session
+      get :new, {}
       assigns(:level).should be_a_new(Level)
     end
   end
@@ -42,7 +38,7 @@ describe LevelsController do
   describe "GET edit" do
     it "assigns the requested level as @level" do
       level = Level.create! valid_attributes
-      get :edit, {:id => level.to_param}, valid_session
+      get :edit, {:id => level.to_param}
       assigns(:level).should eq(level)
     end
   end
@@ -51,18 +47,18 @@ describe LevelsController do
     describe "with valid params" do
       it "creates a new Level" do
         expect {
-          post :create, {:level => valid_attributes}, valid_session
+          post :create, {:level => valid_attributes}
         }.to change(Level, :count).by(1)
       end
 
       it "assigns a newly created level as @level" do
-        post :create, {:level => valid_attributes}, valid_session
+        post :create, {:level => valid_attributes}
         assigns(:level).should be_a(Level)
         assigns(:level).should be_persisted
       end
 
       it "redirects to the created level" do
-        post :create, {:level => valid_attributes}, valid_session
+        post :create, {:level => valid_attributes}
         response.should redirect_to(Level.last)
       end
     end
@@ -71,14 +67,14 @@ describe LevelsController do
       it "assigns a newly created but unsaved level as @level" do
         # Trigger the behavior that occurs when invalid params are submitted
         Level.any_instance.stub(:save).and_return(false)
-        post :create, {:level => {}}, valid_session
+        post :create, {:level => {}}
         assigns(:level).should be_a_new(Level)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Level.any_instance.stub(:save).and_return(false)
-        post :create, {:level => {}}, valid_session
+        post :create, {:level => {}}
         response.should render_template("new")
       end
     end
@@ -93,18 +89,18 @@ describe LevelsController do
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
         Level.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => level.to_param, :level => {'these' => 'params'}}, valid_session
+        put :update, {:id => level.to_param, :level => {'these' => 'params'}}
       end
 
       it "assigns the requested level as @level" do
         level = Level.create! valid_attributes
-        put :update, {:id => level.to_param, :level => valid_attributes}, valid_session
+        put :update, {:id => level.to_param, :level => valid_attributes}
         assigns(:level).should eq(level)
       end
 
       it "redirects to the level" do
         level = Level.create! valid_attributes
-        put :update, {:id => level.to_param, :level => valid_attributes}, valid_session
+        put :update, {:id => level.to_param, :level => valid_attributes}
         response.should redirect_to(level)
       end
     end
@@ -114,7 +110,7 @@ describe LevelsController do
         level = Level.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Level.any_instance.stub(:save).and_return(false)
-        put :update, {:id => level.to_param, :level => {}}, valid_session
+        put :update, {:id => level.to_param, :level => {}}
         assigns(:level).should eq(level)
       end
 
@@ -122,7 +118,7 @@ describe LevelsController do
         level = Level.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Level.any_instance.stub(:save).and_return(false)
-        put :update, {:id => level.to_param, :level => {}}, valid_session
+        put :update, {:id => level.to_param, :level => {}}
         response.should render_template("edit")
       end
     end
@@ -132,13 +128,13 @@ describe LevelsController do
     it "destroys the requested level" do
       level = Level.create! valid_attributes
       expect {
-        delete :destroy, {:id => level.to_param}, valid_session
+        delete :destroy, {:id => level.to_param}
       }.to change(Level, :count).by(-1)
     end
 
     it "redirects to the levels list" do
       level = Level.create! valid_attributes
-      delete :destroy, {:id => level.to_param}, valid_session
+      delete :destroy, {:id => level.to_param}
       response.should redirect_to(levels_url)
     end
   end
