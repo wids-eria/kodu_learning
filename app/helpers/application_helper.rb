3704bf234470d45cf31d487c5bc4d67f33062fa5
Change Me <change@me.org>
2012-02-22 16:59:26 -0600

    Make avatar based on gamer tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ require 'digest/md5'
 
 module ApplicationHelper
   def avatar_for(user)
-    hash = Digest::MD5.hexdigest(user.email)
+    hash = Digest::MD5.hexdigest(user.gamer_tag || user.email)
     image_tag "http://www.gravatar.com/avatar/#{hash}?f=y&d=retro&s=24"
   end
 end
