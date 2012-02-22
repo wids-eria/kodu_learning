f4ace2c86cf5b70d7ed5d7de2aafbbe0ac0bad21
Change Me <change@me.org>
2012-02-22 00:24:55 -0600

    Removed padding, made avatars larger

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ require 'digest/md5'
 module ApplicationHelper
   def avatar_for(user)
     hash = Digest::MD5.hexdigest(user.email)
-    image_tag "http://www.gravatar.com/avatar/#{hash}?f=y&d=retro&s=20"
+    image_tag "http://www.gravatar.com/avatar/#{hash}?f=y&d=retro&s=24"
   end
 end
