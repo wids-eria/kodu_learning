d8a74b256cd0ba1e40541126538b256125a57d58
Change Me <change@me.org>
2012-02-22 17:11:37 -0600

    Add avatar to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 require 'digest/md5'
 
 module ApplicationHelper
-  def avatar_for(user)
+  def avatar_for(user, options = {})
+    options.reverse_merge size: 24
     hash = Digest::MD5.hexdigest(user.gamer_tag || user.email)
-    image_tag "http://www.gravatar.com/avatar/#{hash}?f=y&d=retro&s=24"
+    image_tag "http://www.gravatar.com/avatar/#{hash}?f=y&d=retro&s=#{options[:size]}"
   end
 end
